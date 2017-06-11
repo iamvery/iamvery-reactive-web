10fce800d62f177670c5974c6aa443379cee49a6
Jay Hayes <ur@iamvery.com>
2017-06-11 18:56:57 -0500

    Add basic layout for posts list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Posts extends Component {
+  state = {
+    posts: [{title: 'First Post!'}],
+  };
+
+  renderPost = (post, index) => <li key={index}>{post.title}</li>;
+
+  render() {
+    return (
+      <ul>
+        {this.state.posts.map(this.renderPost)}
+      </ul>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
+      <Posts />
     );
   }
 }
