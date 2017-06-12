5a51217ce1dd3ac00eb5f81a93d36d75ffa5c4c9
Jay Hayes <ur@iamvery.com>
2017-06-11 19:08:45 -0500

    Extract Posts component to its own file

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
 import React, { Component } from 'react';
+import Posts from './Posts.js';
 import './App.css';
-
-class Posts extends Component {
-  state = {
-    loaded: false,
-  };
-
-  renderPost = (post, index) => <li key={index}><a href={post.link}>{post.title}</a></li>;
-
-  componentDidMount() {
-    fetch('https://gist.githubusercontent.com/iamvery/a46ba7af5cf5e017c6865bc8455ca05a/raw/60fae242a4d531a767c154f1d5d07560103e6e09/feed.json')
-      .then((response) => response.json())
-      .then((json) => json.posts)
-      .then((posts) => this.setState({posts, loaded: true}))
-      .catch(console.log)
-  }
-
-  render() {
-    if (this.state.loaded) {
-      return (
-        <ul>
-          {this.state.posts.map(this.renderPost)}
-        </ul>
-      );
-    } else {
-      return (
-        <div>Loading...</div>
-      );
-    }
-  }
-}
 
 class App extends Component {
   render() {
