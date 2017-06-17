d2709fd0c23a9f80a986f89f68e2d6e062dc0b37
Jay Hayes <ur@iamvery.com>
2017-06-17 13:30:46 -0500

    Add test of app content using enzyme shallow rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('greets the world with a hello', () => {
+  const app = shallow(<App />);
+  expect(app.text()).toEqual('Hello, World!');
 });
