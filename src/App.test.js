45798782b19aef39d478bc4c3ff451f315c9e56f
Jay Hayes <ur@iamvery.com>
2017-06-17 13:31:27 -0500

    Add snapshot test to make sure unexpected changes don't sneak in
    I'm interested to see how this plays out over time. These seems like
    they'll be pretty brittle. As such, it may be a bad idea to have them on
    the top-level component. We'll see!

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -11,4 +12,10 @@ it('renders without crashing', () => {
 it('greets the world with a hello', () => {
   const app = shallow(<App />);
   expect(app.text()).toEqual('Hello, World!');
+});
+
+it('does not change unexpectedly', () => {
+  const app = renderer.create(<App />);
+  let tree = app.toJSON();
+  expect(tree).toMatchSnapshot();
 });
