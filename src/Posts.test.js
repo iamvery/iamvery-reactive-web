dddc87b58a59cf60af20dd9d392c1d3ed4b0f16d
Jay Hayes <ur@iamvery.com>
2017-06-20 14:41:05 -0500

    Add snapshot test for Posts component

--- src/Posts.test.js
+++ src/Posts.test.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Posts from './Posts';
 
 it('displays posts', () => {
@@ -7,4 +8,10 @@ it('displays posts', () => {
 
   expect(rendered.text()).toContain('First!');
   expect(rendered.text()).toContain('Another!');
+});
+
+it('does not change unexpectedly', () => {
+  const posts = renderer.create(<Posts data={['one', 'two']} />);
+  let tree = posts.toJSON();
+  expect(tree).toMatchSnapshot();
 });
